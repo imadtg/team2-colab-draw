ae63bb83add6402d6d2225837abf3e1e27a70c61
imadtg <i.haidi@outlook.com>
2025-02-02 20:19:37 +0100

    Another attempt to fix canvas problem

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    // Only apply this fallback in client builds
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
