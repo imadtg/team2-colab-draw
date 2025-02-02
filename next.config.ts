810c470e03eaf914ec649d806ea51d4e6fa9820c
imadtg <i.haidi@outlook.com>
2025-02-02 21:30:23 +0100

    Fix attempt 3

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    // Only apply this fallback in client builds
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        canvas: false,
-      };
-    }
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      canvas: false,
+    };
     return config;
   },
 };
