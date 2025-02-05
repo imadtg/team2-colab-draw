36e205c5baf769fc981d7a2e433e39492bfb5a58
MoussaBOUAKAZ <bouakazmoussa0@gmail.com>
2025-02-05 19:58:02 +0100

    backup removed

--- src/app/backup/page.tsx
+++
@@ -1,9 +0,0 @@
-import ClientCanvas from "@/components/canvas/ClientCanvas";
-
-export default function Home() {
-  return (
-    <div className="text-center">
-      <ClientCanvas />
-    </div>
-  );
-}
